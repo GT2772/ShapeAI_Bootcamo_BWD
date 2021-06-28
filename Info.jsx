644d22d813414d0e287d8bf0c6429a19d2f8312f
Triton777 <86016479+Triton777@users.noreply.github.com>
2021-06-28 19:58:47 +0530

    Add files via upload

---
+++ Info.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Info() {
+  return (
+    <div>
+      <h1 className="note">Javascript and React.js</h1>
+      <p className="note"> A basic web dev with React.js bootcamp</p>
+    </div>
+  );
+}
+
+export default Info;
